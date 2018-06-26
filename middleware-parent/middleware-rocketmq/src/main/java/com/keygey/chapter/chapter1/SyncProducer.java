b5a9c15cbe465776a50aff9905f4827f3e219106
yangkai <yangkaiwj49470@touna.cn>
2018-06-26 18:31:13 +0800

    添加mq实例

---
+++ middleware-parent/middleware-rocketmq/src/main/java/com/keygey/chapter/chapter1/SyncProducer.java
@@ -0,0 +1,26 @@
+package com.keygey.chapter.chapter1;
+
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+
+public class SyncProducer {
+
+	public static void main(String[] args) throws Exception {
+		DefaultMQProducer producer = new DefaultMQProducer("TEST_ROCKET_MQ_GROUP");
+		producer.setNamesrvAddr("193.112.91.102:9876");
+		producer.start();
+		for (int i = 0; i < 100; i++) {
+			 Message msg = new Message("TopicTest" /* Topic */,
+		                "TagA" /* Tag */,
+		                ("Hello RocketMQ " +
+		                    i).getBytes(RemotingHelper.DEFAULT_CHARSET) /* Message body */
+		            );
+			 SendResult result = producer.send(msg);
+			 System.out.printf("%s%n", result);
+		}
+		producer.shutdown();
+	}
+
+}
